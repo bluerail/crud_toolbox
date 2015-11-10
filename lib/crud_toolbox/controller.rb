e255a581f2617b3b8ba14344acd4ca412e7afdba
Martijn Storck <martijn@lico.nl>
2015-11-10 15:46:22 +0100

    Apply style guide using Rubycop
    This destroys most history but since it's only a handful of commits anyway it's worth it

--- lib/crud_toolbox/controller.rb
+++ lib/crud_toolbox/controller.rb
@@ -1,237 +1,216 @@
-module CrudToolbox::Controller
-  extend ActiveSupport::Concern
+module CrudToolbox
+  module Controller
+    extend ActiveSupport::Concern
 
-  require 'pundit'
+    require 'pundit'
 
-  included do
-    include Pundit
+    included do
+      include Pundit
 
-    protect_from_forgery with: :exception
-    before_action :load_resource
-    before_action :set_paths
-  end
-
-
-  # Get the +params+ for this record
-  def record_params
-    return {} if params[record_name].nil?
-
-    if self.use_form_this?
-      params.require(record_name).permit!.merge fixed_params
-    else
-      params.require(record_name).permit self.allowed_fields
-    end
-  end
-
-
-  def fixed_params
-    {}
-  end
-
-
-  def allowed_fields
-    []
-  end
-
-
-  # Apply pagination to the +list+
-  def apply_pagination list
-    list = Kaminari.paginate_array list if list.is_a? Array
-    list.page(params[:page] || 1).per(params[:per])
-  end
-
-
-  # Get the name of the associated ActiveRecord; usually this can be inferred,
-  # but in rare cases you could overwrite it
-  def record_name
-    params[:controller].split('/').pop.downcase.singularize
-  end
-
-
-  # Get the ActiveRecord class
-  def record_class
-    name = self.record_name.capitalize
-    # Replace underscores
-    name.match(/_[a-z]/).to_a.each { |m| name = name.sub m, m[1].upcase }
-    return Object.const_get name
-  end
-
-
-  # Get the FormThis! class
-  def form_class
-    name = self.record_name.capitalize
-    # Replace underscores
-    name.match(/_[a-z]/).to_a.each { |m| name = name.sub m, m[1].upcase }
-    return Object.const_get "#{name}Form"
-  end
-
-
-  # Human readable name
-  def model_title
-    #@_title ||= I18n.t(self.record_class).capitalize
-    @_title ||= _(self.record_class.to_s.titleize.humanize)
-  end
-
-
-  def path_prefix
-  end
-  
-
-  # Assign +@paths+; this function is run once with +before_action+, which is
-  # usually okay. However, if you change +@record+ or +@form+ in your action,
-  # you need to re-run this again to set the correct path.
-  def set_paths record=nil
-    @paths = {
-      form: [self.path_prefix, use_form_this? ? @form : (record || @record)].compact,
-      show: [self.path_prefix, (record || @record)].compact,
-      edit: [:edit, self.path_prefix, (record || @record)].compact,
-      new: [:new, self.path_prefix, self.record_name].compact,
-      index: [self.path_prefix, self.record_name.pluralize].compact,
-    }
-  end
-
-
-  # GET /record/:id
-  def show
-    unless @show_view.present?
-      klass = "ShowView::#{self.record_class}".safe_constantize
-      @show_view = klass.new self, @record unless klass.nil?
+      protect_from_forgery with: :exception
+      before_action :load_resource
+      before_action :set_paths
     end
 
-    respond_to do |format|
-      format.html
-      # TODO: Maybe we want to render the @show_view, rather than the @record?
-      format.json { render json: @record }
-    end
-  end
+    # Get the +params+ for this record
+    def record_params
+      return {} if params[record_name].nil?
 
-
-  # GET /record/:id/edit
-  def edit
-  end
-
-
-  # GET /record/new
-  def new
-  end
-
-
-  # GET /records
-  def index
-    unless @list_view.present?
-      if formats.include?(:json) && params[:tbl_id].present?
-        table_class = params[:tbl_id]
+      if self.use_form_this?
+        params.require(record_name).permit!.merge fixed_params
       else
-        table_class = record_class
+        params.require(record_name).permit allowed_fields
+      end
+    end
+
+    def fixed_params
+      {}
+    end
+
+    def allowed_fields
+      []
+    end
+
+    # Apply pagination to the +list+
+    def apply_pagination(list)
+      list = Kaminari.paginate_array list if list.is_a? Array
+      list.page(params[:page] || 1).per(params[:per])
+    end
+
+    # Get the name of the associated ActiveRecord; usually this can be inferred,
+    # but in rare cases you could overwrite it
+    def record_name
+      params[:controller].split('/').pop.downcase.singularize
+    end
+
+    # Get the ActiveRecord class
+    def record_class
+      name = record_name.capitalize
+      # Replace underscores
+      name.match(/_[a-z]/).to_a.each { |m| name = name.sub m, m[1].upcase }
+      Object.const_get name
+    end
+
+    # Get the FormThis! class
+    def form_class
+      name = record_name.capitalize
+      # Replace underscores
+      name.match(/_[a-z]/).to_a.each { |m| name = name.sub m, m[1].upcase }
+      Object.const_get "#{name}Form"
+    end
+
+    # Human readable name
+    def model_title
+      # @_title ||= I18n.t(self.record_class).capitalize
+      @_title ||= _(record_class.to_s.titleize.humanize)
+    end
+
+    def path_prefix
+    end
+
+    # Assign +@paths+; this function is run once with +before_action+, which is
+    # usually okay. However, if you change +@record+ or +@form+ in your action,
+    # you need to re-run this again to set the correct path.
+    def set_paths(record = nil)
+      @paths = {
+        form: [path_prefix, use_form_this? ? @form : (record || @record)].compact,
+        show: [path_prefix, (record || @record)].compact,
+        edit: [:edit, path_prefix, (record || @record)].compact,
+        new: [:new, path_prefix, record_name].compact,
+        index: [path_prefix, record_name.pluralize].compact
+      }
+    end
+
+    # GET /record/:id
+    def show
+      unless @show_view.present?
+        klass = "ShowView::#{record_class}".safe_constantize
+        @show_view = klass.new self, @record unless klass.nil?
       end
 
-      klass = "ListView::#{record_class}".safe_constantize
-      unless klass.nil?
-        # Ignore params if they're intended for a different class
-        if !formats.include?(:json) && params[:tbl_id] != self.record_class.to_s
-          @list_view = klass.new self, @records
+      respond_to do |format|
+        format.html
+        # TODO: Maybe we want to render the @show_view, rather than the @record?
+        format.json { render json: @record }
+      end
+    end
+
+    # GET /record/:id/edit
+    def edit
+    end
+
+    # GET /record/new
+    def new
+    end
+
+    # GET /records
+    def index
+      unless @list_view.present?
+        klass = "ListView::#{record_class}".safe_constantize
+        unless klass.nil?
+          # Ignore params if they're intended for a different class
+          if !formats.include?(:json) && params[:tbl_id] != record_class.to_s
+            @list_view = klass.new self, @records
+          else
+            @list_view = klass.new self, @records, order: params[:order], filter: params[:filter]
+          end
+        end
+      end
+
+      respond_to do |format|
+        format.html
+        format.json { render json: list_view_json }
+      end
+    end
+
+    # PATCH /record/:id
+    def update
+      if self.use_form_this?
+        success = @form.validate(record_params) && @form.save
+      else
+        success = @record.update record_params
+      end
+
+      respond_to do |format|
+        if success
+          format.html { redirect_to @paths[:index], flash: { notice: update_okay_message } }
+          format.json { head :ok }
         else
-          @list_view = klass.new self, @records, order: params[:order], filter: params[:filter]
+          format.html { render action: 'edit' }
+          format.json { render json: @record.errors, status: :unprocessable_entity }
         end
       end
     end
 
-    respond_to do |format|
-      format.html
-      format.json { render json: list_view_json }
-    end
-  end
-
-
-  # PATCH /record/:id
-  def update
-    if self.use_form_this?
-      success = @form.validate(record_params) && @form.save
-    else
-      success = @record.update record_params
+    def update_okay_message
+      _('%{model_title} ‘%{record}’ has been edited.') % { model_title: model_title, record: @record.to_s }
     end
 
-    respond_to do |format|
-      if success
-        format.html { redirect_to @paths[:index], flash: {notice: self.update_okay_message} }
-        format.json { head :ok }
+    # POST /record
+    def create
+      if self.use_form_this?
+        success = @form.validate(record_params) && @form.save
       else
-        format.html { render action: 'edit' }
-        format.json { render json: @record.errors, status: :unprocessable_entity }
+        success = @record.update record_params
+      end
+
+      respond_to do |format|
+        if success
+          redir_to = if params[:commit_and_add_more].nil?
+                       @paths[:index]
+                     else
+                       polymorphic_path(@paths[:new]) + '?' + (params[:commit_and_add_more].presence || '')
+                     end
+
+          format.html { redirect_to(redir_to, notice: create_okay_message) }
+          format.json { render json: @record, status: :created, location: @record }
+        else
+          format.html { render action: 'new' }
+          format.json { render json: @record.errors, status: :unprocessable_entity }
+        end
       end
     end
-  end
-  def update_okay_message
-    _('%{model_title} ‘%{record}’ has been edited.') % {model_title: self.model_title, record: @record.to_s}
-  end
 
-
-  # POST /record
-  def create
-    if self.use_form_this?
-      success = @form.validate(record_params) && @form.save
-    else
-      success = @record.update record_params
+    def create_okay_message
+      _('%{model_title} ‘%{record}’ created.') % { model_title: model_title, record: @record.to_s }
     end
 
-    respond_to do |format|
-      if success
-        redir_to = if params[:commit_and_add_more].nil?
-                  @paths[:index]
-                else
-                  polymorphic_path(@paths[:new]) + '?' + (params[:commit_and_add_more].presence || '')
-                end
-        format.html { redirect_to(redir_to, notice: self.create_okay_message) }
-        format.json { render json: @record, status: :created, location: @record }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @record.errors, status: :unprocessable_entity }
+    # DELETE /record/:id
+    def destroy
+      respond_to do |format|
+        if @record.destroy
+          format.html { redirect_to @paths[:index], flash: { notice: destroy_okay_message } }
+          format.json { head :ok }
+        else
+          format.html { redirect_to @paths[:show], flash: { alert: destroy_failed_message } }
+          format.json { render json: @record.errors, status: :unprocessable_entity }
+        end
       end
     end
-  end
-  def create_okay_message
-    _('%{model_title} ‘%{record}’ created.') % {model_title: self.model_title, record: @record.to_s}
-  end
 
-
-  # DELETE /record/:id
-  def destroy
-    respond_to do |format|
-      if @record.destroy
-        format.html { redirect_to @paths[:index], flash: { notice: self.destroy_okay_message } }
-        format.json { head :ok }
-      else
-        format.html { redirect_to @paths[:show], flash: { alert: self.destroy_failed_message } }
-        format.json { render json: @record.errors, status: :unprocessable_entity }
-      end
+    def destroy_okay_message
+      _('%{model_title} ‘%{record}’ removed.') % { model_title: model_title, record: @record.to_s }
     end
-  end
-  def destroy_okay_message
-    _('%{model_title} ‘%{record}’ removed.') % {model_title: self.model_title, record: @record.to_s}
-  end
-  def destroy_failed_message
-    _('Removing %{model_title} ‘%{record}’ failed: %{errors}') % {model_title: self.model_title, record: @record.to_s, errors: @record.errors[:base].join(',')}
-  end
 
+    def destroy_failed_message
+      _('Removing %{model_title} ‘%{record}’ failed: %{errors}') % { model_title: model_title, record: @record.to_s, errors: @record.errors[:base].join(',') }
+    end
 
-  def use_form_this?
-    !!defined?(FormThis)
-  end
+    def use_form_this?
+      !!defined?(FormThis)
+    end
 
+    def use_pundit?
+      true
+    end
 
-  def use_pundit?
-    true
-  end
-
-
-  protected
+    protected
 
     def load_resource
       load_resource_all
       build_form if self.use_form_this?
-      self.set_paths if @paths.nil? || @paths[:show] == [self.path_prefix].compact
+      set_paths if @paths.nil? || @paths[:show] == [path_prefix].compact
     end
-
 
     def load_resource_all
       if params[:action] == 'index'
@@ -243,7 +222,6 @@ module CrudToolbox::Controller
       end
     end
 
-
     def load_resource_index
       if use_pundit?
         @records ||= apply_pagination policy_scope(record_class)
@@ -253,8 +231,7 @@ module CrudToolbox::Controller
       instance_variable_set "@#{record_name.pluralize}", @records
     end
 
-
-    def load_resource_single id=nil
+    def load_resource_single(id = nil)
       begin
         @record ||= record_class.find id || params[:id]
       rescue ActiveRecord::RecordNotFound
@@ -271,18 +248,15 @@ module CrudToolbox::Controller
       instance_variable_set "@#{record_name}", @record
     end
 
-
     def load_resource_new
       @record ||= record_class.new
       authorize @record if self.use_pundit?
       instance_variable_set "@#{record_name}", @record
     end
 
-
     def build_form
       @form ||= form_class.new @record if @record.present?
     end
-
 
     def list_view_json
       tbody = render_to_string(
@@ -298,10 +272,11 @@ module CrudToolbox::Controller
         formats: :html
       )
 
-      return {
+      {
         success: true,
         tbody: tbody,
-        buttons: buttons,
+        buttons: buttons
       }
     end
+  end
 end
--- lib/crud_toolbox/show_view.rb
+++ lib/crud_toolbox/show_view.rb
@@ -1,54 +1,52 @@
-module CrudToolbox::ShowView; end
-class CrudToolbox::ShowView::Base
-  attr_accessor :attributes, :header, :record
+module CrudToolbox
+  module ShowView
+    class Base
+      attr_accessor :attributes, :header, :record
 
-  def initialize controller, record
-    @controller = controller
-    @record = record
-    @attributes = []
-    self.set_attributes
-  end
+      def initialize(controller, record)
+        @controller = controller
+        @record = record
+        @attributes = []
+        set_attributes
+      end
 
+      def helpers
+        @controller.view_context
+      end
 
-  def helpers
-    @controller.view_context
-  end
+      def params
+        @controller.params
+      end
 
+      def row(attr, label = nil, blank_value = nil)
+        value = if attr.is_a? Symbol
+                  @record.send attr
+                elsif attr.respond_to? :call
+                  attr
+                else
+                  attr.to_s
+                end
+        label = if label.nil?
+                  helpers.attr_t "#{@controller.record_name}.#{attr}"
+                elsif label.is_a? Symbol
+                  helpers.attr_t "#{@controller.record_name}.#{label}"
+                else
+                  label
+                end
 
-  def params
-    @controller.params
-  end
+        value = blank_value if value.nil?
+        @attributes << {
+          label: label,
+          value: value
+        }
+      end
 
+      def seperator
+        @attributes << { label: nil, value: '&nbsp;'.html_safe }
+      end
 
-  def row attr, label=nil, blank_value=nil
-    value = if attr.is_a? Symbol
-              @record.send attr
-            elsif attr.respond_to? :call
-              attr
-            else
-              attr.to_s
-            end
-    label = if label.nil?
-              helpers.attr_t "#{@controller.record_name}.#{attr.to_s}"
-            elsif label.is_a? Symbol
-              helpers.attr_t "#{@controller.record_name}.#{label.to_s}"
-            else
-              label
-            end
-
-    value = blank_value if value.nil?
-    @attributes << {
-      label: label,
-      value: value,
-    }
-  end
-
-
-  def seperator
-    @attributes << {label: nil, value: '&nbsp;'.html_safe}
-  end
-
-
-  def caption
+      def caption
+      end
+    end
   end
 end
